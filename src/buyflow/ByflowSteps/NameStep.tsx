d96a03f9ebdd46ff5a7ec7cb6a6fb1e2925a0117
Vadym Zazhyrko <vadym.zazhyrko@gmail.com>
2023-05-11 16:15:04 +0200

    [refactor] Update name step with ref

--- src/buyflow/ByflowSteps/NameStep.tsx
+++ src/buyflow/ByflowSteps/NameStep.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { NameStepProps } from '../../types/types';
 
 
 const NameStep: React.FC<NameStepProps> = ({ onNext }) => {
+
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+
   const handleNextClick = () => {
-    const firstName = (document.getElementById('firstName') as HTMLInputElement).value;
-    const lastName = (document.getElementById('lastName') as HTMLInputElement).value;
-    if (firstName === ''  || lastName === '') {
+    const firstNameInput = firstNameRef.current;
+    const laststNameInput = lastNameRef.current;
+
+    if (!firstNameInput
+      || !laststNameInput
+      || !firstNameInput.value.trim()
+      || !laststNameInput.value.trim()
+      ) {
       alert('Please enter your name');
       return;
     }
-    onNext(`${firstName} ${lastName}`);
+    onNext(`${firstNameInput.value.trim()} ${laststNameInput.value.trim()}`);
   }
 
   return (
     <>
       <div>
         <label htmlFor="firstName">First name:{' '}</label>
-        <input type="text" id="firstName" />
+        <input type="text" id="firstName" ref={firstNameRef}/>
       </div>
       <div>
         <label htmlFor="lastName">Last name:{' '}</label>
-        <input type="text" id="lastName" />
+        <input type="text" id="lastName" ref={lastNameRef} />
       </div>
       <button onClick={handleNextClick}>Next</button>
     </>
